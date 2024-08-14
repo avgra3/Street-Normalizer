a1f2ede709cef9d6ee421d491afeb59a95826759
Antony G. <50510640+avgra3@users.noreply.github.com>
2024-08-14 14:18:33 -0700

    Update __init__.py

---
+++ src/street_normalizer/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from .street_normalizer import normalize_street_suffixes, normalize_unit_designators, normalize_street_abbreviations, remove_extra_chars, cleaner, city_cleaner
+
+__all__ = (
+    "normalize_street_suffixes",
+    "normalize_unit_designators",
+    "normalize_street_abbreviations",
+    "remove_extra_chars",
+    "cleaner",
+    "city_cleaner",
+)
