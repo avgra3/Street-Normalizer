8359aa3ded5db130095de99bb90829e46fd3a0d0
Antony <antonygradillas@gmail.com>
2024-08-26 10:14:37 -0700

    Cleaned method for easier read

--- src/street_normalizer/street_normalizer.py
+++ src/street_normalizer/street_normalizer.py
@@ -50,7 +50,13 @@ def remove_extra_chars(initial_value: str) -> str:
 
 
 def cleaner(initial_value: str) -> str:
-    return normalize_street_abbreviations(normalize_street_suffixes(normalize_unit_designators(remove_extra_chars(initial_value))))
+    return normalize_street_abbreviations(
+        normalize_street_suffixes(
+            normalize_unit_designators(
+                remove_extra_chars(initial_value)
+            )
+        )
+    )
 
 
 def city_cleaner(city: str) -> str:
